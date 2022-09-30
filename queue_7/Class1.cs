785b5cee080956073ddf94a4811ad8682c5b0f51
Mohithbellamkonda <88969197+Mohithbellamkonda@users.noreply.github.com>
2022-09-30 23:36:03 +0530

    Add files via upload

---
+++ queue_7/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+class person
+{
+    public string name;
+    public int age;
+    public ArrayList c = new ArrayList();
+    public Queue<ArrayList> a = new Queue<ArrayList>();
+    public void Tostring()
+    {
+        c.Add(name);
+        c.Add((int)age);
+        a.Enqueue(c);
+        Console.WriteLine("name  age");
+        foreach (ArrayList item in a)
+        {
+            foreach (var v in item)
+            {
+                Console.Write(v + " ");
+            }
+        }
+
+    }
+
+}
---
+++ queue_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace queque
+{
+   
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            person Person=new person();
+            Person.age = 22;
+            Person.name = "hari";
+            Person.Tostring();
+        }
+    }
+}
